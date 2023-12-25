52d7a6ce47250900dcd2d1922b60ffae30b787ab
Kendy <kndysong@gmail.com>
2023-12-25 15:05:38 +0100

    Add new integral animation

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Animation script for math videos with utils
 
 <img src="https://github.com/KendySong/math-anim/blob/main/assets/example.gif"></img>
+
+## Run the compilation of a scene
+```git
+manim -pqh -r 1080,1920 integrals.py Integral_1
+```
--- integrals.py
+++ integrals.py
@@ -1,16 +1,63 @@
 from manim import *
+import config
+
+class Integral_1(Scene) :
+    def construct(self):
+        self.camera.background_color = GRAY
+
+        integral = MathTex(r'\int_{-\infty}^{-2} \frac{1}{(x+1)^3} \,dx', font_size=config.TEX_SIZE)
+        self.play(Create(integral))
+        self.play(integral.animate.shift(UP * 7))
+
+        uSub = MathTex(r'u = x + 1', font_size=config.TEX_SIZE).shift(UP * 4)
+        duSub =  MathTex(r'dx = du', font_size=config.TEX_SIZE).next_to(uSub, direction=DOWN).align_to(uSub, direction=LEFT)
+        self.play(FadeIn(uSub))
+        self.play(FadeIn(duSub))
+
+        subIntegral = MathTex(r'\int_{-\infty}^{-1} \frac{1}{u^3} \,dx', font_size=config.TEX_SIZE)
+        self.play(
+            FadeIn(subIntegral), 
+            FadeOut(integral),
+            FadeOut(uSub),
+            FadeOut(duSub)
+        )
+
+        equal = MathTex('=', font_size=config.TEX_SIZE).shift(UP)
+        self.play(
+            subIntegral.animate.shift(UP * 4),
+            FadeIn(equal)  
+        )
+        primitive = MathTex(r'\left(-\frac{1}{2x^2}\right)\bigg|^{-1}_{-\infty}', font_size=config.TEX_SIZE).next_to(equal, direction=DOWN*2).shift(LEFT*0.50)
+        self.play(FadeIn(primitive))
+
+        self.play(
+            FadeOut(subIntegral),
+            FadeOut(equal)
+        )
+
+        self.play(
+            primitive.animate.shift(UP * 4.5),
+            FadeIn(equal)
+        )
+
+        result = MathTex(r'-\frac{1}{2}', font_size=config.TEX_SIZE).next_to(equal, direction=DOWN*2)
+        self.play(
+            Transform(primitive, result), 
+            FadeOut(equal)
+        )
+        self.wait()
 
 class Integral_0(Scene) :
     def construct(self):
-        baseIntegral = MathTex(r'\int_{-1}^{1} \frac{5}{(2x+3)^2} \,dx', font_size=96).shift(LEFT * 3)
+        baseIntegral = MathTex(r'\int_{-1}^{1} \frac{5}{(2x+3)^2} \,dx', font_size=config.TEX_SIZE).shift(LEFT * 3)
         uSub_u = MathTex(r'u = 2x + 3', font_size=72).shift(UP * 2).next_to(baseIntegral, direction=UP + RIGHT*4)
         uSub_du = MathTex(r'du = 2 \,dx', font_size=72).next_to(uSub_u, direction=DOWN).align_to(uSub_u, direction=LEFT)
         uSub_dx =  MathTex(r'dx = \frac{du}{2}', font_size=72).next_to(uSub_du, direction=DOWN).align_to(uSub_u, direction=LEFT)
 
-        subIntegral = MathTex(r'\int_{1}^{5} \frac{5}{u^2}\, \frac{du}{2}', font_size=96)
-        subFracIntegral = MathTex(r'\frac{5}{2} \int_{1}^{5} \frac{1}{u^2}\, du', font_size=96)
-        equal = MathTex('=', font_size=96)
-        primitive = MathTex(r'\frac{5}{2} (-\frac{1}{5} + \frac{5}{5})', font_size=96).next_to(equal, direction=DOWN*1.5)
+        subIntegral = MathTex(r'\int_{1}^{5} \frac{5}{u^2}\, \frac{du}{2}', font_size=config.TEX_SIZE)
+        subFracIntegral = MathTex(r'\frac{5}{2} \int_{1}^{5} \frac{1}{u^2}\, du', font_size=config.TEX_SIZE)
+        equal = MathTex('=', font_size=config.TEX_SIZE)
+        primitive = MathTex(r'\frac{5}{2} (-\frac{1}{5} + \frac{5}{5})', font_size=config.TEX_SIZE).next_to(equal, direction=DOWN*1.5)
 
 
         self.play(Create(baseIntegral))
@@ -41,7 +88,7 @@ class Integral_0(Scene) :
         self.play(Create(primitive))
         
         self.play(FadeOut(subFracIntegral), primitive.animate.move_to(ORIGIN+LEFT*2), equal.animate.shift(RIGHT))
-        self.play(Create(MathTex('2', font_size=96).next_to(equal, direction=RIGHT)))
+        self.play(Create(MathTex('2', font_size=config.TEX_SIZE).next_to(equal, direction=RIGHT)))
 
         self.wait(0.2)
 
